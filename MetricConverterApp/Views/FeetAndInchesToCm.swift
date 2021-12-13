0e6d51e80a8888525f1a85adb190b6861cad3006
Luke Newbigging <lnewbigging@lcs.on.ca>
2021-12-13 14:54:20 -0500

    Starting Commit, created Three views that I will complete for my app.

---
+++ MetricConverterApp/Views/FeetAndInchesToCm.swift
@@ -0,0 +1,20 @@
+//
+//  FeetAndInchesToCm.swift
+//  MetricConverterApp
+//
+//  Created by Luke Newbigging on 2021-12-13.
+//
+
+import SwiftUI
+
+struct FeetAndInchesToCm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeetAndInchesToCm_Previews: PreviewProvider {
+    static var previews: some View {
+        FeetAndInchesToCm()
+    }
+}
---
+++ MetricConverterApp/Views/GallonsToLiters.swift
@@ -0,0 +1,20 @@
+//
+//  GallonsToLiters.swift
+//  MetricConverterApp
+//
+//  Created by Luke Newbigging on 2021-12-13.
+//
+
+import SwiftUI
+
+struct GallonsToLiters: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GallonsToLiters_Previews: PreviewProvider {
+    static var previews: some View {
+        GallonsToLiters()
+    }
+}

1d2c288b4f99e5463d8b92b1fb8f38debca5ecfd
Luke Newbigging <lnewbigging@lcs.on.ca>
2021-12-13 14:51:16 -0500

    Initial Commit

---
+++ MetricConverterApp/MetricConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetricConverterAppApp.swift
+//  MetricConverterApp
+//
+//  Created by Luke Newbigging on 2021-12-13.
+//
+
+import SwiftUI
+
+@main
+struct MetricConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

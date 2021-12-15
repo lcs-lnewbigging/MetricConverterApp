25ba32879f069fbc9ba0db7581a8aacfa50d5193
Luke Newbigging <lnewbigging@lcs.on.ca>
2021-12-15 10:00:15 -0500

    Have made amazing process on the list view, added a great image as a banner and made a good looking landing page.

--- MetricConverterApp/Views/ListView.swift
+++ MetricConverterApp/Views/ListView.swift
@@ -9,12 +9,78 @@ import SwiftUI
 
 struct ListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            
+            VStack{
+                
+                Image("banner2")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(10)
+                
+                
+                Text("Follow Each page to use the helpful converting interface!")
+                    .bold()
+                    .font(.title2)
+                
+                Divider()
+                
+                Group {
+                    NavigationLink(destination: {
+                        KgsToLbs()
+                    }, label: {
+                        Text("Weight Converter")
+                            .bold()
+                            .font(.title3)
+                    })
+                }
+                Divider()
+                
+                Group {
+                    NavigationLink(destination: {
+                        FeetAndInchesToCm()
+                    }, label: {
+                        Text("Measurement Converter")
+                            .bold()
+                            .font(.title3)
+                    })
+                }
+                
+                Divider()
+                
+                Group {
+                    NavigationLink(destination: {
+                        MilesToKm()
+                    }, label: {
+                        Text("Kilometers To Miles")
+                            .bold()
+                            .font(.title3)
+                    })
+                }
+                
+                Divider()
+                
+                Group {
+                    NavigationLink(destination: {
+                        GallonsToLiters()
+                    }, label: {
+                        Text("Liters to Gallons")
+                            .bold()
+                            .font(.title3)
+                    })
+                }
+                
+                
+            }
+        }
+//        .navigationTitle("The Converter App")
     }
 }
-
 struct ListView_Previews: PreviewProvider {
     static var previews: some View {
-        ListView()
+        NavigationView {
+            ListView()
+        }
+        .preferredColorScheme(.dark)
     }
 }
